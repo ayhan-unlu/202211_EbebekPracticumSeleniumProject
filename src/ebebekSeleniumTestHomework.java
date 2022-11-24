93546362bdf23094c62e98f69230f3459f4cebd7
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2022-11-24 05:13:22 +0300

    Update ebebekSeleniumTestHomework.java

--- src/ebebekSeleniumTestHomework.java
+++ src/ebebekSeleniumTestHomework.java
@@ -11,6 +11,8 @@ public class ebebekSeleniumTestHomework {
         System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\SeleniumProject\\src\\drivers\\chromedriver.exe");//mentioning the driver we use and indicating its location
         WebDriver driver = new ChromeDriver();//for driver initilisation or creating a driver object in java
         driver.get(baseUrl);//to indicate where you want to go https:// must be included
+     // driver.get("https://www.e-bebek.com");//to indicate where you want to go https:// must be included not obligatory to assign a variable like baseUrl
+
 
         // source : https://www.youtube.com/watch?v=sxmwGdZbV-Y&t=3s
 
@@ -21,7 +23,7 @@ public class ebebekSeleniumTestHomework {
         driver.manage().window().minimize();//maximize the window
         Thread.sleep(1000);//waits for 1 second to go to next line of code
 
-        driver.findElement(By.id("txtSearchBox")).sendKeys("biberon",Keys.ENTER);
+        driver.findElement(By.id("txtSearchBox")).sendKeys("biberon",Keys.ENTER);//finds the id for search bar and enters biberon-Baby bottle word for searching
 
         Thread.sleep(1000);//waits for 1 second to go to next line of code
 
@@ -29,18 +31,16 @@ public class ebebekSeleniumTestHomework {
         driver.findElement(By.xpath("body > app-root > cx-storefront > main > cx-page-layout > cx-page-slot.SearchResultsListSlot.has-components > eb-product-list > div > section > div.container.p-0 > div > div > div.cx-product-container.row > eb-product-scroll > div > div > eb-product-list-item:nth-child(1) > div > eb-generic-link > a")).click();
 
         //body/app-root[1]/cx-storefront[1]/main[1]/cx-page-layout[1]/cx-page-slot[3]/eb-product-list[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[2]/eb-product-scroll[1]/div[1]/div[1]/eb-product-list-item[1]/div[1]/eb-generic-link[1]/a[1]/figure[1]/cx-media[1]/img[1]
-
+        //xpath is not working properly throws "no such element" error
         Thread.sleep(10000);//waits for 1 second to go to next line of code
 
        
 
        // Thread.sleep(10000);//waits for 1 second to go to next line of code
 
-       // driver.get("https://www.e-bebek.com");//to indicate where you want to go https:// must be included
 
-        System.out.println(driver.getTitle());//for printing website title
-        Thread.sleep(10000);//waits for 1 second to go to next line of code
-     //   driver.quit();//for leaving the website
+        Thread.sleep(10000);//waits for 10 second to go to next line of code
+        driver.quit();//for leaving the website
         
 
     }
